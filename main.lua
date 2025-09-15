5706f0b1f26b5e0e36228d5a0078a337f8543093
Tsukisashii <jinthong04@gmail.com>
2025-09-15 12:18:17 +0800

    [Auto] Update main.lua with latest servers

--- main.lua
+++ main.lua
@@ -10,7 +10,7 @@ local TextChatService = game:GetService("TextChatService")
 local player = Players.LocalPlayer
 
 local RIFTS = {
-    {Name = "lunar-egg", Webhook = "https://discord.com/api/webhooks/1400470775592521798/-48oF_gPY30E3FhwJY8djz4k6eD64ZOgaWCmKHWzf9cW1nQN53WUoq0aOmd2JGWRJXWP", MinLuck = 5},  
+    {Name = "lunar-egg", Webhook = "https://discord.com/api/webhooks/1415712364007002143/p80C7QElE5O1EEDo-0IKJA5cGiG31O8qlEBQ1dgmibyOtO2Fr228CK7-JQiM2vpLb8Mz", MinLuck = 25},  
     {Name = "brainrot-egg", Webhook = "https://discord.com/api/webhooks/1415718364055077025/_cblNWmsQS35E-1xCz-CQWYMbiKm4aFncF_0ngpDsavEPFPbfL5QUE1nP7kmk2xWzy1V", MinLuck = nil}  
 }
 
